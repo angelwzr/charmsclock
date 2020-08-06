027f15a1dbc7e3727cef52c21886dff34618c416
angelwzr <angel@windowz.ru>
2020-08-06 19:21:34 +0300

    Tray icon, keyboard shortcut

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System;
+﻿using NHotkey;
+using NHotkey.Wpf;
+using System;
+using System.Linq;
 using System.Windows;
 using System.Windows.Input;
 using System.Windows.Threading;
@@ -10,49 +13,101 @@ namespace ChClock
     /// </summary>
     public partial class MainWindow : Window
     {
-        DispatcherTimer timer;
+        private DispatcherTimer timer;
 
         public MainWindow()
         {
             InitializeComponent();
 
-            timer = new DispatcherTimer();
-            timer.Interval = TimeSpan.FromSeconds(1.0);
-            timer.Start();
-            timer.Tick += new EventHandler(delegate (object s, EventArgs a)
+            timer = new DispatcherTimer
             {
-
+                Interval = TimeSpan.FromSeconds(1.0)
+            };
+            timer.Start();
+            timer.Tick += new EventHandler(delegate(object s, EventArgs a)
+            {
                 time.Text = DateTime.Now.ToString("HH") + ":"
-              + DateTime.Now.ToString("mm");
+                                                        + DateTime.Now.ToString("mm");
 
                 day.Text = "" + DateTime.Now.DayOfWeek;
 
                 date.Text = DateTime.Now.ToString("MMMM") + " " + DateTime.Now.Day;
             });
+
+            //Implementing hotkeys
+            HotkeyManager.Current.AddOrReplace("Show or hide", Key.C, ModifierKeys.Control | ModifierKeys.Shift,
+                OnHotKey);
         }
 
         private void Window_Activated(object sender, EventArgs e)
         {
-            this.Topmost = true;
+            Topmost = true;
         }
 
         private void Window_Deactivated(object sender, EventArgs e)
         {
-            Window window = (Window)sender;
+            var window = (Window) sender;
             window.Topmost = true;
-            this.Activate();
+            Activate();
         }
 
         private void Window_MouseDown(object sender, MouseButtonEventArgs e)
         {
             if (e.ChangedButton == MouseButton.Left)
-                this.DragMove();
+                DragMove();
         }
 
         private void Window_PreviewLostKeyboardFocus(object sender, KeyboardFocusChangedEventArgs e)
         {
-            Window window = (Window)sender;
+            var window = (Window) sender;
             window.Topmost = true;
+        }
+
+        private void OnHotKey(object sender, HotkeyEventArgs e)
+        {
+            TrayIconClick(sender, null);
+        }
+
+        private void TrayIconClick(object sender, RoutedEventArgs e)
+        {
+            if (Application.Current.MainWindow.IsVisible)
+                Application.Current.MainWindow.Hide();
+            else
+                Application.Current.MainWindow.Show();
+        }
+
+        //Settings menu item
+        private void Settings_Click(object sender, RoutedEventArgs e)
+        {
+            var settingsWindow = Application.Current.Windows.OfType<SettingsWindow>().FirstOrDefault();
+            if (settingsWindow == null)
+            {
+                settingsWindow = new SettingsWindow();
+                settingsWindow.Show();
+            }
+            else
+            {
+                settingsWindow.Focus();
+            }
+        }
+
+        //Single instance management for app windows
+        public static void OpenWindow<T>() where T : Window
+        {
+            var windows = Application.Current.Windows.Cast<Window>();
+            var any = windows.Any(s => s is T);
+            if (any)
+            {
+                var subWindow = windows.Where(s => s is T).ToList()[0];
+                if (subWindow.WindowState == WindowState.Minimized)
+                    subWindow.WindowState = WindowState.Normal;
+                subWindow.Focus();
+            }
+            else
+            {
+                var subWindow = (Window) Activator.CreateInstance(typeof(T));
+                subWindow.Show();
+            }
         }
 
         private void Exit_Click(object sender, RoutedEventArgs e)
